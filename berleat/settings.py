a346409c4fbe520eada86f47140ab49b57b0253e
TrisKast <tristan.kast@gmx.de>
2020-06-24 21:51:39 +0200

    changend url routing

--- berleat/settings.py
+++ berleat/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mlmdzqlvc(tee#gdl_$)u#809lwvrq_s*wc(p(sjwbhn55d=yv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 
-#ALLOWED_HOSTS = ['berl-eat.herokuapp.com', '127.0.0.1']
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['berl-eat.herokuapp.com', '127.0.0.1']
+#ALLOWED_HOSTS = ['127.0.0.1', ]
 
 
 # Application definition
@@ -81,12 +81,14 @@ WSGI_APPLICATION = 'berleat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
--- general/urls.py
+++ general/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing, name='landing'),
+    #path('', views.landing, name='landing'),
+    path('test_landing/', views.landing, name='landing'),
 ]
